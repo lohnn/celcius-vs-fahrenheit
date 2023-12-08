178c9e3c001bba9237a87771dd7689537de401f6
joel <joel.as.0808@gmail.com>
2023-12-08 23:11:17 +0100

    arrowstuff

--- lib/components/arrow.dart
+++ lib/components/arrow.dart
@@ -1,10 +1,32 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
+import 'package:flame/palette.dart';
 
-class Arrow extends PositionComponent {
-  Vector2 fromPos;
-  Vector2 toPos;
-
+class Arrow extends Component {
+  final Vector2 fromPos;
+  final Vector2 toPos;
+  Paint arrowPaint = BasicPalette.blue.paint();
   Arrow({
     required this.fromPos,
   }) : toPos = fromPos;
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawLine(fromPos.toOffset(), toPos.toOffset(), arrowPaint);
+    const scale = 5.0;
+    final dir1 = toPos - Vector2(fromPos.x + toPos.x, fromPos.y);
+    final dir2 = toPos - Vector2(fromPos.x, fromPos.y + toPos.y);
+    canvas.drawLine(
+      (dir1.clone()..scaleTo(scale)).toOffset(),
+      toPos.toOffset(),
+      arrowPaint,
+    );
+    canvas.drawLine(
+      (dir2.clone()..scaleTo(scale)).toOffset(),
+      toPos.toOffset(),
+      arrowPaint,
+    );
+    super.render(canvas);
+  }
 }
